341c3e1b6bce6674b2782b77ab037090b277e3dc
Claudio Soto <claudio91soto@gmail.com>
2022-04-11 16:23:35 -0300

    Ejercicios JS OB

---
+++ JS_Basico/Ejercicio1/app.js
@@ -0,0 +1,2 @@
+//Ejercicio 1 Tema 1
+console.log('Estoy realizando el curso de JavaScript, espero aprender mucho');
---
+++ JS_Basico/Ejercicio2/app.js
@@ -0,0 +1,21 @@
+// En este ejercicio tienes que crear un archivo JavaScript donde crees una variable la cual contenga una cadena de texto.
+
+// Después crearás una función, dentro de ella crearas una variable que se llame igual que la primera que has creado y le asignaras una cadena de texto diferente a la primera y mostrarás la variable por consola.
+
+// Por último, fuera de la función tendrás que realizar la llamada a la función y mostrarás por consola la primera variable que has creado.
+
+//Solución
+//Variable que contiene una cadena de texto
+
+let cadenatxt = 'Soy una cadena de texto';
+
+function funcion1(){
+    let cadenatxt = 'Cadena de texto diferente';
+
+    console.log(cadenatxt);
+}
+//Llamo a la funcion
+funcion1();
+
+//Primer variable
+console.log(cadenatxt);
